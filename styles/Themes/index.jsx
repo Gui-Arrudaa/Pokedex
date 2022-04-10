3b41f773d609843115cb45e341480fe39b314976
Guilherme <guipanfiglio123@outlook.com>
2022-04-10 18:05:33 -0300

    Create Themes

---
+++ styles/Themes/index.jsx
@@ -0,0 +1,12 @@
+export const Light = {
+  bodyBg: "#f6f8fa",
+  bg: "#fff",
+  color: "#212529"
+};
+
+export const Dark = {
+  bodyBg: "#24292e",
+  bg: "#2f363d",
+  color: "rgba(255, 255, 255, 0.87)",
+  placeholder: "rgba(255, 255, 255, 0.6)"
+};
