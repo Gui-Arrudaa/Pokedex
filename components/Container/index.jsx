a8a9149ad2631b96bf1224323298554d0b9bb464
Guilherme <guipanfiglio123@outlook.com>
2022-04-11 00:13:39 -0300

    Create Container component

---
+++ components/Container/index.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 1.5rem 3rem 3rem;
+
+  @media screen and (max-width: 575.98px) {
+    padding: 1.5rem 1.5rem 3rem;
+  }
+`;
+
+export default Container;
